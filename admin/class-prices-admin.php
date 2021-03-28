ac486448ec32345f4701b2b4ab5798040250107e
baldurarge <baldurargesveins@gmail.com>
2021-03-28 11:36:44 +0200

    removed updater

--- admin/class-prices-admin.php
+++ admin/class-prices-admin.php
@@ -109,29 +109,6 @@ class Prices_Admin {
 }
 
 
-
-include_once('updater.php');
-
-
-if (is_admin()) { // note the use of is_admin() to double check that this is happening in the admin
-	$config = array(
-		'slug' => plugin_basename(__FILE__), // this is the slug of your plugin
-		'proper_folder_name' => 'prices', // this is the name of the folder your plugin lives in
-		'api_url' => 'https://api.github.com/repos/baldurarge/HRON-Prices', // the GitHub API url of your GitHub repo
-		'raw_url' => 'https://raw.github.com/baldurarge/HRON-Prices/master', // the GitHub raw url of your GitHub repo
-		'github_url' => 'https://github.com/baldurarge/HRON-Prices.git', // the GitHub url of your GitHub repo
-		'zip_url' => 'https://github.com/baldurarge/HRON-Prices/zipball/master', // the zip url of the GitHub repo
-		'sslverify' => true, // whether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
-		'requires' => '3.0', // which version of WordPress does your plugin require?
-		'tested' => '5.7', // which version of WordPress is your plugin tested up to?
-		'readme' => 'README.md', // which file to use as the readme for the version number
-		'access_token' => '4a50e029436871aa067b7db01ae4698d9f5d4dc5', // Access private repositories by authorizing under Plugins > GitHub Updates when this example plugin is installed
-	);
-	new WP_GitHub_Updater($config);
-}
-
-
-
 function price_admin_page_add() {
 
 
